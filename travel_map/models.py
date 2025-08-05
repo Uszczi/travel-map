fdda084f97fe630b56de4d1fca47f0f80e4a249a
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-08-05 15:57:40 +0200

    ✨ add total_new total_old and percent_of_new

--- travel_map/models.py
+++ travel_map/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
 
 
 class Segment(BaseModel):
@@ -15,6 +15,23 @@ class Route(BaseModel):
     elevation: list[int]
     total_gain: int
     total_lose: int
+
+    @computed_field(return_type=float)
+    @property
+    def total_new(self) -> float:
+        return sum(s.distance for s in self.segments if s.new)
+
+    @computed_field(return_type=float)
+    @property
+    def total_old(self) -> float:
+        return sum(s.distance for s in self.segments if not s.new)
+
+    @computed_field(return_type=float)
+    @property
+    def percent_of_new(self) -> float:
+        if self.distance == 0:
+            return 0.0
+        return (self.total_new / self.distance) * 100
 
 
 class StravaRoute(BaseModel):
