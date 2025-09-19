bf9f183ac67c90a979f8e39bccc348df96044e78
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-09-19 18:08:37 +0200

    🚀 add run migrations

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -6,4 +6,5 @@ DIRNAME=$(basename "$PWD")
 cp /root/pp/.env.$DIRNAME ./.env
 
 docker compose -f ./deployment/docker-compose.yml build
+docker compose -f ./deployment/docker-compose.yml run --rm --no-deps app alembic upgrade head
 docker compose -f ./deployment/docker-compose.yml up --detach
