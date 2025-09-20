4eae4b00bae4f9ba3069a7908a66df6a1d7a102c
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-09-20 11:07:58 +0200

    🧑‍💻 extract lifespan

--- travel_map/app.py
+++ travel_map/app.py
@@ -1,23 +1,9 @@
-from collections.abc import AsyncIterator
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-from redis import asyncio as aioredis
 
 from travel_map.api import include_routers, init_sentry, setup_middlewares
+from travel_map.extensions.lifespan import lifespan
 from travel_map.extensions.prometheus import include_prometheus
 from travel_map.settings import settings
-
-
-# TODO move this
-@asynccontextmanager
-async def lifespan(_: FastAPI) -> AsyncIterator[None]:
-    redis = aioredis.from_url(settings.REDIS_URL)
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
-    yield
-
 
 init_sentry()
 
