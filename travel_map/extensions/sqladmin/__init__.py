5385a668ce07936384da997c1db5d96edc72346c
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-09-21 11:53:15 +0200

    🐛 try to fix backoffice

--- travel_map/extensions/sqladmin/__init__.py
+++ travel_map/extensions/sqladmin/__init__.py
@@ -3,9 +3,10 @@ from sqladmin import Admin
 
 from travel_map.db import engine
 from travel_map.extensions.sqladmin.user import UserAdmin
+from travel_map.settings import settings
 
 
 def include_sqladmin(app: FastAPI) -> None:
-    admin = Admin(app, engine, base_url="/backoffice")
+    admin = Admin(app, engine, base_url=settings.URL_PREFIX + "/backoffice")
 
     admin.add_view(UserAdmin)
