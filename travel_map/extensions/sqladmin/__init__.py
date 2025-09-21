8b241c600b91f96bc53ede3c6d921af5b115c313
Mateusz Papuga <mateuszpapuga24@gmail.com>
2025-09-21 13:28:18 +0200

    🐛 try to fix

--- travel_map/extensions/sqladmin/__init__.py
+++ travel_map/extensions/sqladmin/__init__.py
@@ -3,15 +3,22 @@ from sqladmin import Admin
 
 from travel_map.db import engine
 from travel_map.extensions.sqladmin.user import UserAdmin
-from travel_map.settings import settings
+
+# from travel_map.settings import settings
 
 
 def include_sqladmin(app: FastAPI) -> None:
-    if settings.URL_PREFIX:
-        base_url = f"/{settings.URL_PREFIX}/backoffice"
-    else:
-        base_url = "/backoffice"
+    # if settings.URL_PREFIX:
+    #     if settings.URL_PREFIX.startswith("/"):
+    #         base_url = f"{settings.URL_PREFIX}/backoffice"
+    #     else:
+    #         base_url = f"/{settings.URL_PREFIX}/backoffice"
+    # else:
+    base_url = "/backoffice"
 
+    print(base_url)
+    print(base_url)
+    print(base_url)
     admin = Admin(app, engine, base_url=base_url)
 
     admin.add_view(UserAdmin)
