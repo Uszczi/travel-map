0ddd1685a5bc24fc9b3f4ae110468eaf09ddc907
Mateusz Papuga <mateuszpapuga24@gmail.com>
2026-05-11 16:30:47 +0200

    remove credentails

--- scripts/check-mikrus.sh
+++ scripts/check-mikrus.sh
@@ -3,8 +3,8 @@
 local_travel_map=$(git rev-parse --short HEAD)
 local_travel_map_fe=$(cd ../travel-map-fe && git rev-parse --short HEAD)
 
-mikrus_travel_map=$(curl https://api.mateuszpapuga.pl/version -s | jq -r .version)
-mikrus_travel_map_fe=$(curl https://mateuszpapuga.pl/api/version -s | jq -r .version)
+mikrus_travel_map=$(curl https://api.DOMAIN.pl/version -s | jq -r .version)
+mikrus_travel_map_fe=$(curl https://DOMAIN.pl/api/version -s | jq -r .version)
 
 echo "Local travel-map    : $local_travel_map"
 echo "Online travel-map   : $mikrus_travel_map"
